5a67959a0dfa3e924f08c8d98743f1096c46830d
arsch808 <42979009+arsch808@users.noreply.github.com>
2025-10-16 13:11:43 -0700

    Integrating pygame_gui
    Using pygame_gui for ui elements; better arrangement of elements for 1-screen layout

--- gooeytest.py
+++ gooeytest.py
@@ -1,56 +1,81 @@
 import pygame
-from pygame.locals import *
+import pygame_gui
 
-# pygame setup
+#initialize and set up pygame
 pygame.init()
-pygame.display.set_caption("World Explorer")
-screen = pygame.display.set_mode((1280, 720))
+pygame.display.set_caption('World Explorer')
 clock = pygame.time.Clock()
-running = True
+is_running = True
 
-cursor = pygame.image.load('PLACEHOLDER_cursor.bmp').convert_alpha() #placeholder mouse cursor
-background = pygame.image.load('stars.bmp').convert() #placeholder background
-mars = pygame.image.load('mars.bmp')
 
-#header position
-header = Rect((200, 50), (800, 100))
+#default window surface. everything should be attached to here unless otherwise stated
+screen = pygame.display.set_mode((1280, 720))
+manager = pygame_gui.UIManager((1280, 720))
 
-#define Rect object for the slider to use
-sizebox = Rect((20, 600), (100, 10))
-distbox = Rect((220, 600), (100, 10))
-waterbox = Rect((420, 600), (100, 10))
-atmosbox = Rect((620, 600), (100, 10))
+#create a display background
+background = pygame.image.load('stars.bmp').convert() #placeholder background, convert() makes the image easier to handle
+cursor = pygame.image.load('PLACEHOLDER_cursor.bmp').convert_alpha()
 
-#setting a clipping box to limit what draws on the screen
-#screen.set_clip(0,0,640,300) #starting at x,y, following call will only draw w/i (640,300)
-#draw_area()
 
-color = [127,127,127]
+#inputs
+sizeslide = pygame_gui.elements.ui_horizontal_slider.UIHorizontalSlider(
+    ((20, 650), (200, 25)), 0, (0,3), manager, None,None, 'slider', None, 1, 1)
+distslide = pygame_gui.elements.ui_horizontal_slider.UIHorizontalSlider(
+    ((330, 650), (200, 25)), 0, (0,3), manager, None,None, 'slider', None, 1, 1)
+denslide = pygame_gui.elements.ui_horizontal_slider.UIHorizontalSlider(
+    ((690, 650), (200, 25)), 0, (0,3), manager, None,None, 'slider', None, 1, 1)
+tempslide = pygame_gui.elements.ui_horizontal_slider.UIHorizontalSlider(
+    ((1010 , 650), (200, 25)), 0, (0,3), manager, None,None, 'slider', None, 1, 1)
 
-#font = pygame.font.SysFont('Arial', 16)
-#title = font.render("Welcome to the Worlds Explorer", True, (255, 255, 255), background=None)
+#display image
+mars = pygame.image.load('mars.bmp').convert()
 
-while running:
-#DEFINE EVENTS HERE
+image = pygame_gui.elements.ui_image.UIImage(
+    ((25, 100), (500, 500)), mars)
+
+#"checkbox" for if a parameter matches
+dist_checkbox = pygame.Rect((640, 120), (150, 100))
+size_checkbox = pygame.Rect((790, 120), (150, 100))
+dens_checkbox = pygame.Rect((940, 120), (150, 100))
+temp_checkbox = pygame.Rect((1090, 120), (150, 100))
+
+
+#text 
+title_label = pygame_gui.elements.UILabel(
+    relative_rect=pygame.Rect((330,20), (600, 100)),
+    text="Worlds Explorer",
+    manager=manager,
+    object_id='title_label')
+
+textRect = pygame.Rect((640, 240), (620,200))
+
+#set_image(new_image: ~pygame.surface.Surface) allows image to change during runtime (screensaver)
+
+
+while is_running:
+
+    #DEFINE EVENTS HERE
+    time_delta = clock.tick(60)/1000.0
     for event in pygame.event.get():
-        if event.type == pygame.QUIT: #user clicks x to close the window
-            running = False   
+        if event.type == pygame.QUIT:
+            is_running = False
 
-    # wipe last frame
-    screen.fill("black")
 
-    screen.blit(background, (0,0))
+        manager.process_events(event)
 
-    #drawing control placeholders onto the screen
-    pygame.draw.rect(screen, (255,255,255), sizebox)
-    pygame.draw.rect(screen, (255,255,255), distbox)
-    pygame.draw.rect(screen, (255,255,255), waterbox)
-    pygame.draw.rect(screen, (255,255,255), atmosbox)
+    manager.update(time_delta)
 
-    pygame.draw.rect(screen, (255, 255, 255), header)
-
+    #drawing onto the screen
+    screen.blit(background, (0, 0))
+    manager.draw_ui(screen)
     
-    
+    pygame.draw.rect(screen, 'white', dist_checkbox)
+    pygame.draw.rect(screen, 'black' , size_checkbox)
+    pygame.draw.rect(screen, 'white', dens_checkbox)
+    pygame.draw.rect(screen, 'black', temp_checkbox)
+
+    pygame.draw.rect(screen, 'white', textRect)
+
     mouse = pygame.mouse.get_pos()
     click = pygame.mouse.get_pressed()
     
@@ -58,18 +83,10 @@ while running:
     x -= cursor.get_width()/2
     y -= cursor.get_height()/2
     screen.blit(cursor, (x, y))
-
-
-
     
-    
-#END: this handles the final display
 
+#END: final display
     pygame.display.update()
 
-    clock.tick(60)  #FPS 60
 
 pygame.quit()
-
-
-
