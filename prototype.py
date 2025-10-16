30cbe97215667def3e62fc180ec6f218ab98a850
arsch808 <42979009+arsch808@users.noreply.github.com>
2025-10-16 13:14:14 -0700

    setup for csv implement
    add compare() function (note: still needs work) and reformat input options

--- prototype.py
+++ prototype.py
@@ -1,110 +1,123 @@
-import random
+import csv
 
 #defining the main 4x4 attributes
 #STRINGS
 #right now these are strings but i have the feeling as they are linked to a button press they will become a 0,1 integer of some sort 
 #really just making my life harder rn i think for the sake of understanding my own code
-Water = ["Desert", "Lakes", "Ocean", "Megaocean"]
 Distance = ["Close", "Inner", "Outer", "Distant" ]
 Size = ["Small", "Medium", "Giant", "Supergiant" ]
-Atmosphere = ["Thin & Light", "Thin & Dense", "Thick & Light", "Thick & Dense" ]
+Density = ["Solid", "Varied", "Watery", "Puffy"]
+Temp = ["Cold", "Varied", "Temperate", "Hot"]
 
 
 
 #hab scores for each attribute, uses these for the final score
-hab_water = (1, 2, 3, 3)
+hab_temp = (1, 2, 3, 1)
 hab_distance = (1, 3, 2, 1)
 hab_size = (1, 3, 3, 2)
-hab_atmosphere = (1, 2, 3, 1)
-
-def hab_score(w, d, s, a):
-    score = hab_water[Water.index(w)] + hab_distance[Distance.index(d)] + hab_size[Size.index(s)] + hab_atmosphere[Atmosphere.index(a)]
-    return score
-
-
-
-#pull facts for each attribute, several facts per attribute 
-#water
-desertfact = ["1desert", "2desert", "3desert"]
-lakefact = ["1lake", "2lake", "3lake"]  
-oceanfact = ["1ocean", "2ocean", "3ocean"]      
-megafact = ["1mega", "2mega", "3mega"] 
-
-#distance
-closefact = ["1close", "2close", "3close"]
-innerfact = ["1inner", "2inner", "3inner"]
-outerfact = ["1outer", "2outer", "3outer"]
-distantfact = ["1distant", "2distant", "3distant"]
-
-#size
-smallfact = ["1small", "2small", "3small"]
-mediumfact = ["1medium", "2medium", "3medium"]
-giantfact = ["1giant", "2giant", "3giant"]
-superfact = ["1super", "2super", "3super"]
-
-#atmosphere
-thinlightfact = ["1thinlight", "2thinlight", "3thinlight"]
-thindensefact = ["1thindense", "2thindense", "3thindense"]
-thicklightfact = ["1thicklight", "2thicklight", "3thicklight"]
-thickdensefact = ["1thickdense", "2thickdense", "3thickdense"]
-
-#creates a new list of ALL facts of all chosen attributes, stores this in variable factsheet
-def create_factsheet(w, d, s, a):
-    factsheet = []
-    if w == "Desert":
-        factsheet.extend(desertfact)
-    elif w == "Lakes":
-        factsheet.extend(lakefact)
-    elif w == "Ocean":
-        factsheet.extend(oceanfact)
-    elif w == "Megaocean":
-        factsheet.extend(megafact)
-
-    if d == "Close":
-        factsheet.extend(closefact)
-    elif d == "Inner":
-        factsheet.extend(innerfact)
-    elif d == "Outer":
-        factsheet.extend(outerfact)
-    elif d == "Distant":
-        factsheet.extend(distantfact)
-
-    if s == "Small":
-        factsheet.extend(smallfact)
-    elif s == "Medium":
-        factsheet.extend(mediumfact)
-    elif s == "Giant":
-        factsheet.extend(giantfact)
-    elif s == "Supergiant":
-        factsheet.extend(superfact)
-
-    if a == "Thin & Light":
-        factsheet.extend(thinlightfact)
-    elif a == "Thick & Light":
-        factsheet.extend(thindensefact)
-    elif a == "Thin & Dense":
-        factsheet.extend(thicklightfact)
-    elif a == "Thick & Dense":
-        factsheet.extend(thickdensefact)
-    return factsheet
-
-#randomly selects 3 facts from the factsheet to show, stores in displaysheet
-def create_displaysheet(factsheet):
-    displaysheet = random.sample(factsheet, 3)
-    return displaysheet    
-
+hab_density = (1, 2, 3, 1)
 
 
 def main():
-        w = input("Choose Water:")
-        d = input("Choose Distance:")
-        s = input("Choose Size:")
-        a = input("Choose Atmosphere:")
-        score = hab_score(w, d, s, a)
-        factsheet = create_factsheet(w, d, s, a)
-        displaysheet = create_displaysheet(factsheet)
-        print("Hab Score:", score)
-        #print(factsheet)
-        print("Display Facts:", displaysheet)
+    d = input("Distance:")
+    p = input("Density:")
+    s = input("Size:")
+    t = input("Temp:")
+
+    dscore = pscore = sscore = tscore = 0
+    dlabel = plabel = slabel = tlabel = "Invalid"
+
+    try:
+        d = int(d)
+        p = int(p)
+        s = int(s)
+        t = int(t)
+    except ValueError:
+        print("Please enter numbers 1-4 for each attribute.")
+        return
+
+    if d == 1:
+        dlabel = "Close"
+        dscore = 1
+    elif d == 2:
+        dlabel = "Inner"
+        dscore = 3
+    elif d == 3:
+        dlabel = "Outer"
+        dscore = 2
+    elif d == 4:
+        dlabel = "Distant"
+        dscore = 1
+    else:
+        print("Invalid Distance input.")
+
+    if p == 1:
+        plabel = "Solid"
+        pscore = 1
+    elif p == 2:
+        plabel = "Varied"
+        pscore = 2
+    elif p == 3:
+        plabel = "Watery"
+        pscore = 3
+    elif p == 4:
+        plabel = "Puffy"
+        pscore = 1
+    else:
+        print("Invalid Density input.")
+    
+    if s == 1:
+        slabel = "Small"
+        sscore = 1
+    elif s == 2:
+        slabel = "Medium"
+        sscore = 3
+    elif s == 3:
+        slabel = "Giant"
+        sscore = 3
+    elif s == 4:
+        slabel = "Supergiant"
+        sscore = 2
+    else:
+        print("Invalid Size input.")
+
+    if t == 1:
+        tlabel = "Cold"
+        tscore = 1
+    elif t == 2:
+        tlabel = "Varied"
+        tscore = 2
+    elif t == 3:
+        tlabel = "Temperate"
+        tscore = 3
+    elif t == 4:
+        tlabel = "Hot"
+        tscore = 1
+    else:
+        print("Invalid Temp input.")
+
+    score = dscore + pscore + sscore + tscore
+    print('parameters:', dlabel, plabel, slabel, tlabel)
+    print("Hab Score:", score)
 
 main()
+
+def compare(file, target):
+    best_match_array = None
+    max_matches = -1
+    target = [d, p, s, t]
+
+    with open('ss_test.csv', 'r', newline='') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            current_matches = 0
+            for item in target:
+                if item in row:
+                    current_matches += 1
+
+            if current_matches > max_matches:
+                max_matches = current_matches
+                best_match_array = row
+    return best_match_array
+
+compare(file, target)
